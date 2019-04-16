cdea7eb3e6a13f9018007acbf67da6d6dbe0166a
YUSUKE TANABE <e12707@kmjp.local>
2019-04-16 14:18:26 +0900

    修正前（Slackで送っていただいたコード）

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
 const questionDesc = document.getElementById('question-desc');
 const answerArea = document.getElementById('answer-area');
+let index = 0;
+const playerAnswer = [];
+let correct;
 
-fetch('https://opentdb.com/api.php?amount=10')
- .then(response => response.json())
- .then(data => {
- 	//問題文の取得
-  	const question = data.results.map(item => item.question);
+function quiz(data){
+			//問題に全て答えていない時の処理
+		if(data.results.length > index){
+				 	//問題文の取得
+		  	const question = data.results.map(item => item.question);
 
-  	//間違った答え(ダミー選択肢)を取得
-  	const incorrectAnswers = data.results.map(item => item.incorrect_answers);
+		  	//間違った答え(ダミー選択肢)を取得
+		  	const incorrectAnswers = data.results.map(item => item.incorrect_answers);
+		  	
 
-  	//正解の答えを取得
-  	const correctAnswer = data.results.map(item => item.correct_answer);
+		  	//正解の答えを取得
+		  	const correctAnswer = data.results.map(item => item.correct_answer);
+		  	correct = correctAnswer;
+		  	//ダミーの選択肢と正解の選択肢を結合した配列
+		  	const answers = [];
 
-  	//正解数を判別するための配列
-  	const playerAnswer = [];
+		  	//ダミーの選択肢と正解の選択肢を結合する処理
+		  	for(num = 0; num < correctAnswer.length; num++){
+		  		answers.push(incorrectAnswers[num].concat(correctAnswer[num]));	
+		  	}
 
-  	//ダミーの選択肢と正解の選択肢を結合した配列
-  	const answers = [];
-
-  	//ダミーの選択肢と正解の選択肢を結合する処理
-  	for(num = 0; num < correctAnswer.length; num++){
-  		answers.push(incorrectAnswers[num].concat(correctAnswer[num]));	
-  	}
-
-  	let i = 0;
-  	
-  	console.log(answers);
-
-  	function quiz(){
-  		//問題に全て答えていない時の処理
-		if(data.results.length > i){
-				questionDesc.innerHTML = question[i];
-		  		answers[i].forEach(item =>{
+			questionDesc.innerHTML = question[index];
+	  		answers[index].forEach(item =>{
 		  		let button = document.createElement('button');
 		  		button.innerHTML = item;
 				answerArea.appendChild(button);
@@ -46,30 +40,34 @@ fetch('https://opentdb.com/api.php?amount=10')
 
 					//不要な選択肢の削除
 					answerArea.textContent = '';
-					i++;
-					quiz();
+					index++;
+					quiz(data);
 				});
-	  		});
-	  	}
+			});
+		}
 
-	  	//問題に答え終わった後の処理
-	  	else{
-	  		//正解した問題数を代入する変数
-	  		let yes = 0;
+		//問題に答え終わった後の処理
+		else{
+			//正解した問題数を代入する変数
+			let yes = 0;
 
-	  		//不正解の問題数を代入する変数
-	  		let no = 0;
+			//不正解の問題数を代入する変数
+			let no = 0;
+			
+			playerAnswer.forEach((element, index) => {
+				if (element == correct[index]){
+					yes++;
+				}
+				else{
+					no++;
+				}
+			});
+			questionDesc.innerHTML = `正解した問題は${yes}問です。不正解の問題は${no}問です。`
+		}
+}
 
-	  		for(i = 0; i < playerAnswer.length; i++){
-	  			if(playerAnswer[i] == correctAnswer[i]){
-	  				yes++;
-	  			}
-	  			else{
-	  				no++;
-	  			}
-	  		}
-	  		questionDesc.innerHTML = `正解した問題は${yes}問です。不正解の問題は${no}問です。`
-	  	}
-  	}
-  	quiz();
+fetch('https://opentdb.com/api.php?amount=10')
+ .then(response => response.json())
+ .then(data => {
+  	quiz(data);
  });
